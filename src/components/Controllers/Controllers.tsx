e2d8fd8dca12abdae707f3b17fa79f0c57352b85
stevebarakat <stevebarakat@gmail.com>
2025-05-31 04:15:31 -0400

    ci

--- src/components/Controllers/Controllers.tsx
+++ src/components/Controllers/Controllers.tsx
@@ -41,19 +41,17 @@ function Controllers() {
           <Knob
             value={glideTime}
             min={0}
-            max={1}
+            max={10}
             step={0.01}
             label="Glide Time"
             onChange={setGlideTime}
             valueLabels={{
-              "-7": "-7",
-              "-5": "-5",
-              "-3": "-3",
-              "-1": "-1",
-              "1": "1",
-              "3": "3",
-              "5": "5",
-              "7": "7",
+              "0": "0",
+              "2": "2",
+              "4": "4",
+              "6": "6",
+              "8": "8",
+              "10": "10",
             }}
           />
           <Knob
