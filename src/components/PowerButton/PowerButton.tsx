b43cd04109812d217fb103e2f9fb2d5134c1da22
stevebarakat <stevebarakat@gmail.com>
2025-06-12 22:03:08 -0400

    ci

--- src/components/PowerButton/PowerButton.tsx
+++ src/components/PowerButton/PowerButton.tsx
@@ -1,37 +1,37 @@
 import React from "react";
-import { Power } from "lucide-react";
-import styles from "./PowerButton.module.css";
+import VerticalRockerSwitch from "../RockerSwitch/VerticalRockerSwitch";
 
 interface PowerButtonProps {
   isOn: boolean;
   onPowerOn: () => void;
   onPowerOff: () => void;
-  className?: string;
 }
 
 const PowerButton: React.FC<PowerButtonProps> = ({
   isOn,
   onPowerOn,
   onPowerOff,
-  className,
 }) => {
-  const handleClick = React.useCallback(() => {
-    if (isOn) {
-      onPowerOff();
-    } else {
-      onPowerOn();
-    }
-  }, [isOn, onPowerOff, onPowerOn]);
+  const handleCheckedChange = React.useCallback(
+    (checked: boolean) => {
+      if (checked) {
+        onPowerOn();
+      } else {
+        onPowerOff();
+      }
+    },
+    [onPowerOn, onPowerOff]
+  );
 
   return (
-    <button
-      className={`${styles.powerButton} ${className || ""}`}
-      aria-pressed={isOn}
-      aria-label={isOn ? "Power Off" : "Power On"}
-      onClick={handleClick}
-    >
-      <Power className={styles.icon} />
-    </button>
+    <VerticalRockerSwitch
+      checked={isOn}
+      onCheckedChange={handleCheckedChange}
+      label="Power"
+      theme="orange"
+      topLabel="ON"
+      bottomLabel="OFF"
+    />
   );
 };
 
