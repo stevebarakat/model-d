10f71c3fd337636938c65d2fedfa8fcf18950fb5
stevebarakat <stevebarakat@gmail.com>
2025-06-24 15:51:06 -0400

    ci

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -3,22 +3,37 @@ import React from "react";
 type SectionProps = {
   children: React.ReactNode;
   style?: React.CSSProperties;
+  justify?: "flex-start" | "center" | "flex-end" | "space-between";
+  align?: "flex-start" | "center" | "flex-end" | "space-between";
 };
 
 const defaultStyle: React.CSSProperties = {
   position: "relative",
   display: "flex",
   flexDirection: "column",
-  justifyContent: "space-between",
-  alignItems: "center",
-  gap: 0,
   padding: "2rem 0 1rem",
   borderRight: "2px solid var(--color-white-50)",
   height: "100%",
 };
 
-function Section({ children, style }: SectionProps) {
-  return <div style={{ ...defaultStyle, ...style }}>{children}</div>;
+function Section({
+  children,
+  style,
+  justify = "flex-end",
+  align = "center",
+}: SectionProps) {
+  return (
+    <div
+      style={{
+        ...defaultStyle,
+        ...style,
+        justifyContent: justify,
+        alignItems: align,
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Section;
