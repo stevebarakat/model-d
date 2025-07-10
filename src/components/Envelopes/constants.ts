9e414f2d9605c0da4f9affedd1c7fa19db03e372
stevebarakat <stevebarakat@gmail.com>
2025-07-10 03:22:47 -0400

    ci

--- src/components/Envelopes/constants.ts
+++ src/components/Envelopes/constants.ts
@@ -2,7 +2,7 @@ import { valueToKnobPos } from "../Knob/utils/attackDecayMapping";
 
 // Define label values and map to knob positions
 export const attackDecayValueLabels = Object.fromEntries([
-  [valueToKnobPos(1), "m-sec."],
+  [valueToKnobPos(0), "m-sec."],
   [valueToKnobPos(10), "10"],
   [valueToKnobPos(200), "200"],
   [valueToKnobPos(600), "600"],
